78be8c77a2d3e51e9091f7a8a9f5bd30cb0b163a
Justin Quiros <jquiros2290@gmail.com>
2017-11-27 14:25:32 -0600

    Ruby assignments

---
+++ justin_quiros/exercise1.rb
@@ -0,0 +1,44 @@
+=begin
+
+
+This is how you 
+comment out multiple lines. 
+
+=end
+
+
+
+
+
+first_name = "Justin"
+last_name = "Quiros"
+
+# puts "Your name is " +first_name+" "+last_name
+# puts "First name is #{first_name} and last name is #{last_name}"
+# puts "First name is %s and last name is %s" % [first_name, last_name]
+z = 50
+puts "Value of z is #{z}"
+puts "Value of z is %d" % z
+
+puts "I am 6'1\" tall"
+
+# puts "hello"
+# puts "Coding"
+# puts "Dojo"
+
+# # print "hello"
+# # print "Coding"
+# x = 5
+# y = 10
+# z = x+y
+
+# puts z
+
+
+# BEGIN {
+# 	puts "this runs before my puts"
+# }
+
+# END {
+# 	puts "this runs after my puts"
+# }
