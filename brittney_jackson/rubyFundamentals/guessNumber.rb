8a0f8865efe7af3439595e9773d1e8c7614c9218
Brittney <b.nychole87@gmail.com>
2017-11-27 19:17:05 -0600

    guessnumber

--- brittney_jackson/rubyFundamentals/guessNumber.rb
+++ brittney_jackson/rubyFundamentals/guessNumber.rb
@@ -4,11 +4,11 @@ def guess_number guess
     number = 25
     # your code here
     if guess > 25
-    	puts 'Guess was too high'
+    	return 'Guess was too high'
     elsif guess < 25
-    	puts 'Guess was too low'
+    	return 'Guess was too low'
     else
-    	puts 'You Got it'
+    	return 'You Got it'
     end
 
 end 
