fc63d286f053b81a03770d863c2c17099d1b0456
Brittney <b.nychole87@gmail.com>
2017-11-27 19:13:54 -0600

    inital commit

---
+++ brittney_jackson/rubyFundamentals/guessNumber.rb
@@ -0,0 +1,14 @@
+
+
+def guess_number guess
+    number = 25
+    # your code here
+    if guess > 25
+    	puts 'Guess was too high'
+    elsif guess < 25
+    	puts 'Guess was too low'
+    else
+    	puts 'You Got it'
+    end
+
+end 
