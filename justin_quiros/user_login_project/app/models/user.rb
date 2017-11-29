82724dd1484edfc3133011a0c2766afd98220334
Justin Quiros <jquiros2290@gmail.com>
2017-11-29 13:28:49 -0600

    User/Login

--- justin_quiros/user_login_project/app/models/user.rb
+++ justin_quiros/user_login_project/app/models/user.rb
@@ -3,6 +3,5 @@ class User < ActiveRecord::Base
 	validates :first_name, :last_name, :email, :age, presence: true
 	validates :first_name, :last_name, length: { in: 2..50 }
 	validates :age, numericality: { greater_than_or_equal_to: 10, less_than: 150 }
-	validates_numericality_of :age, less_than: 150
 	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }, length: { in: 4..40 }
 end
