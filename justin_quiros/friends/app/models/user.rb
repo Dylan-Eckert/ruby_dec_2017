790e87d4a71107f539a3082d2cc2dcd7c3c8b4aa
Justin Quiros <jquiros2290@gmail.com>
2017-12-01 14:06:13 -0600

    Routes

--- justin_quiros/friends/app/models/user.rb
+++ justin_quiros/friends/app/models/user.rb
@@ -2,6 +2,4 @@ class User < ActiveRecord::Base
 	has_many :homies, class_name: 'Homie'
 	has_many :friends, through: :homies, source: :receiver
 
-	# has_many :received_friends, class_name: 'Homies', foreign_key: 'receiver_id'
-
 end
