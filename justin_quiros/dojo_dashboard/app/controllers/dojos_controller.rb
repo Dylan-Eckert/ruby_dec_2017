35a467dfd1fbe5b69df81af754f664a0fc3a82e5
Justin Quiros <jquiros2290@gmail.com>
2017-12-04 07:35:10 -0600

    Dojo Dashboard III

--- justin_quiros/dojo_dashboard/app/controllers/dojos_controller.rb
+++ justin_quiros/dojo_dashboard/app/controllers/dojos_controller.rb
@@ -2,7 +2,7 @@ class DojosController < ApplicationController
   def index
   	@dojos = Dojo.all
   end
-  
+
   def new
   	@dojo = Dojo.new
   end
@@ -17,6 +17,33 @@ class DojosController < ApplicationController
   		flash[:errors] = @dojo.errors.full_messages
   		redirect_to dojos_new_path
   	end
+  end
+
+  def show
+  	@dojo = Dojo.find(params[:id])
+  end
+
+  def edit
+  	@dojo = Dojo.find(params[:id])
+  	
+  end
+
+  def update
+  	dojo = Dojo.find(params[:id])
+  	
+  	if dojo.update(form_params)
+  		flash[:success] = "You have successfully updated a Dojo!"
+		redirect_to dojos_path
+	else
+		flash[:errors] = dojo.errors.full_messages
+		redirect_to :back
+	end
+  end
+
+  def destroy
+  	dojo = Dojo.find(params[:id])
+  	dojo.destroy
+  	redirect_to dojos_path
   end
 
   private
